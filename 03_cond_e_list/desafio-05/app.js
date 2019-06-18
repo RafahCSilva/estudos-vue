b3155c6e6c5543a4c63e234b8dc9c9a530cefa6a
RafahCSilva <rafahcsilva@gmail.com>
2019-06-18 16:55:26 -0300

    03: Condicionais e Listas

---
+++ 03_cond_e_list/desafio-05/app.js
@@ -0,0 +1,19 @@
+new Vue( {
+  el: '#desafio',
+  data: {
+    ex1: true,
+    ex2: [ 1, 2, 3, 4, 132, 41324, 23, 4234, 325, 32, 5, 23 ],
+
+    array: [ 'Pedro', 'Bia', 'Ana', 'Rebeca' ],
+    livro: {
+      titulo: 'O Senhor dos Anéis',
+      autor: 'J.R.R. Tolkiens',
+      volume: '3'
+    },
+    aluno: {
+      id: 10,
+      nome: 'Maria',
+      notas: [ 7.67, 8.33, 6.98, 9.21 ]
+    }
+  }
+} )
