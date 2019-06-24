01ae53d625a554aab24bf050ad75a516099e0899
RafahCSilva <rafahcsilva@gmail.com>
2019-06-24 16:54:16 -0300

    07: Intro aos Componentes - Desafio Organizando Componentes

---
+++ 07_componentes/componente-desafio/src/main/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+
+    <app-menu></app-menu>
+
+    <app-toolbar></app-toolbar>
+
+    <v-content>
+      <app-carousel></app-carousel>
+    </v-content>
+
+    <Footer/>
+
+  </v-app>
+</template>
+
+<script>
+  import Menu from '../componentes/template/Menu'
+  import Toolbar from '../componentes/template/Toolbar'
+  import Carousel from '../componentes/widgets/Carousel'
+  import Footer from '../componentes/template/Footer'
+
+  export default {
+    components: {
+      appMenu: Menu,
+      appToolbar: Toolbar,
+      appCarousel: Carousel,
+      Footer,
+    },
+  }
+</script>
