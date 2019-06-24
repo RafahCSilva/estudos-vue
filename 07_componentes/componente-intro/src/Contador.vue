d96ea2aed2ef8422fafe481b77fbf4df9ae1d5ff
RafahCSilva <rafahcsilva@gmail.com>
2019-06-24 16:28:04 -0300

    07: Intro aos Componentes - Criando um Componente

---
+++ 07_componentes/componente-intro/src/Contador.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="contador">
+   <span>{{ contador }}</span>
+  <button @click="contador++">+</button>
+  <button @click="contador--">-</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Contador',
+    data () {
+      return {
+        contador: 0
+      }
+    },
+    methods: {
+      adicionar () {
+        this.contador++
+      },
+      subtrair () {
+        this.contador--
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .contador span {
+    border-bottom: 1px solid #CCC;
+    height: 30px;
+    padding: 5px 25px;
+  }
+
+  .contador button {
+    height: 30px;
+    width: 30px;
+    border-radius: 15px;
+    background-color: coral;
+    color: white;
+    outline: none;
+  }
+</style>
