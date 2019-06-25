74e94082d1b90af54f9147ff27a88e3824c2b157
RafahCSilva <rafahcsilva@gmail.com>
2019-06-25 15:58:03 -0300

    13: Melhorando sua App com Filtros e Mixin - Mixin

--- 13_filtros_mixins/filtros-mixins-exercicios/src/App.vue
+++ 13_filtros_mixins/filtros-mixins-exercicios/src/App.vue
@@ -2,19 +2,37 @@
   <div id="app">
     <h1>Filtros & Mixins</h1>
     <hr>
+    <p>{{ usuarioLogado }}</p>
     <p>{{ u_cpf | f_cpf | f_inverter }}</p>
     <p>{{ u_cpf | f_inverter }}</p>
     <p>{{ 'ASDFG' | f_inverter }}</p>
     <hr>
     <input type="text" :value="u_cpf | f_cpf | f_inverter">
+    <hr>
+    <hr>
+    <Frutas></Frutas>
+    <hr>
+    <div>
+      <ul>
+        <li v-for="fruta in frutas" :key="fruta">{{ fruta }}</li>
+      </ul>
+      <input type="text" v-model="fruta" @keydown.enter="add">
+    </div>
   </div>
 </template>
 
 <script>
+  import Frutas from './Frutas'
+  import FrutasMixin from './FrutasMixin'
+  import usuarioMixin from './usuarioMixin'
+
   export default {
+    components: { Frutas },
+    mixins: [ FrutasMixin, usuarioMixin ],
     data() {
       return {
         u_cpf: '12312312300',
+        frutas: [ 'abacate' ],
       }
     },
     filters: {
@@ -40,6 +58,6 @@
   }
 
   input {
-    font-size: 2.5rem;
+    font-size: 1.5rem;
   }
 </style>
