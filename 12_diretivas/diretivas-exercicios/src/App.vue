451f56a2a9efe4358c667e11c01714699d13aec7
RafahCSilva <rafahcsilva@gmail.com>
2019-06-25 15:06:19 -0300

    12: Usando e Criando Diretivas - Global/Local e Arg/Mod

--- 12_diretivas/diretivas-exercicios/src/App.vue
+++ 12_diretivas/diretivas-exercicios/src/App.vue
@@ -1,11 +1,87 @@
-<template>
+<template xmlns:v-destaque="http://www.w3.org/1999/xhtml">
   <div id="app">
     <h1>Diretivas</h1>
+    <hr>
+    <p v-text="'Usando diretiva <strong>v-text</strong>'"></p>
+    <p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
+    <hr>
+    <p v-destaque="'lightblue'">Usando Diretiva personalizada</p>
+    <p v-destaque="cor">Usando Diretiva personalizada</p>
+    <p v-destaque:fundo.atrasar="'purple'">Usando Diretiva personalizada</p>
+    <p v-destaque-local:fundo.atrasar.alternar="'grey'">Usando Diretiva personalizada</p>
+    <hr>
+    <p v-destaque-local2:fundo.atrasar.alternar="{ cor1:'green', cor2:'red', intervalo:200 }">Usando Diretiva personalizada Complexa</p>
+    <p v-destaque-local2.atrasar="{ cor1:'red', atraso: 2500 }">Usando Diretiva personalizada Complexa</p>
+    <hr>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    directives: {
+      'destaque-local': {
+        bind( el, binding, vNode ) {
+
+          const aplicarCor = ( cor ) => {
+            if ( binding.arg === 'fundo' ) {
+              el.style.backgroundColor = cor
+            } else {
+              el.style.color = cor
+            }
+          }
+
+          let atraso = binding.modifiers[ 'atrasar' ] ? 2000 : 0
+
+          let cor1 = binding.value
+          let cor2 = 'purple'
+          let corAtual = cor1
+
+          setTimeout( () => {
+            if ( binding.modifiers[ 'alternar' ] ) {
+              setInterval( () => {
+                corAtual = corAtual === cor1 ? cor2 : cor1
+                aplicarCor( corAtual )
+              }, 1000 )
+            } else {
+              aplicarCor( binding.value )
+            }
+          }, atraso )
+        },
+      },
+      'destaque-local2': {
+        bind( el, binding, vNode ) {
+          const aplicarCor = ( cor ) => {
+            if ( binding.arg === 'fundo' ) {
+              el.style.backgroundColor = cor
+            } else {
+              el.style.color = cor
+            }
+          }
+
+          let atraso = binding.modifiers[ 'atrasar' ] ? binding.value.atraso : 0
+          let cor1 = binding.value.cor1
+          let cor2 = binding.value.cor2
+          let corAtual = cor1
+
+          setTimeout( () => {
+            if ( binding.modifiers[ 'alternar' ] ) {
+              setInterval( () => {
+                corAtual = corAtual === cor1 ? cor2 : cor1
+                aplicarCor( corAtual )
+              }, binding.value.intervalo )
+            } else {
+              aplicarCor( cor1 )
+            }
+          }, atraso )
+        },
+      },
+    },
+    data() {
+      return {
+        cor: 'red',
+      }
+    },
+  }
 </script>
 
 <style>
--- 12_diretivas/diretivas-exercicios/src/main.js
+++ 12_diretivas/diretivas-exercicios/src/main.js
@@ -3,6 +3,22 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive( 'destaque', {
+  bind( el, binding, vNode ) {
+    // el.style.backgroundColor = 'lightgreen'
+
+    let atraso = binding.modifiers[ 'atrasar' ] ? 3000 : 0
+
+    setTimeout( () => {
+      if ( binding.arg === 'fundo' ) {
+        el.style.backgroundColor = binding.value
+      } else {
+        el.style.color = binding.value
+      }
+    }, atraso )
+  },
+} )
+
 new Vue( {
   render: h => h( App ),
 } ).$mount( '#app' )
