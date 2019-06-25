a3709b3f80b1def0d919029c125a9333d6faa63d
RafahCSilva <rafahcsilva@gmail.com>
2019-06-25 17:51:15 -0300

    14: Adicionando Animações e Transições - CSS

--- 14_animacao_transicao/animacoes-exercicios/src/App.vue
+++ 14_animacao_transicao/animacoes-exercicios/src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <div id="app" class="container-fluid">
     <h1>Animações</h1>
+    <hr>
+    <b-button
+      variant="primary"
+      class="mb-4"
+      @click="exibir = !exibir"
+    >
+      Mostrar Mensagem
+    </b-button>
+    <transition name="fade">
+      <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+    </transition>
+    <transition name="slide" type="animation">
+      <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+    </transition>
+    <transition name="slide" type="animation" appear>
+      <b-alert variant="info" show v-if="true">{{ msg }}</b-alert>
+    </transition>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        msg: 'Uma mensagem de informação para o usuario!',
+        exibir: false,
+      }
+    },
   }
 </script>
 
-<style>
+<style lang="scss">
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -18,5 +41,58 @@
     color: #2c3e50;
     margin-top: 60px;
     font-size: 1.5rem;
+  }
+
+  /* FADE */
+  .fade {
+    &-enter,
+    &-leave-to {
+      opacity: 0;
+    }
+
+    &-enter-active,
+    &-leave-active {
+      transition: opacity 1s;
+    }
+  }
+
+
+  /* SLIDE */
+  @keyframes slide-in {
+    from {
+      transform: translateY(40px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(40px);
+    }
+  }
+
+  .slide {
+    &-enter-active {
+      animation: slide-in 1s ease;
+    }
+
+    &-leave-active {
+      animation: slide-out 1s linear;
+    }
+
+    &-enter,
+    &-leave-to {
+      opacity: 0;
+    }
+
+    &-enter-active,
+    &-leave-active {
+      transition: opacity 1s;
+    }
   }
 </style>
