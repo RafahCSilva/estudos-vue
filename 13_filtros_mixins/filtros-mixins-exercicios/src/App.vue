c27069dc116282a8891e82599e8596b2aad0e21a
RafahCSilva <rafahcsilva@gmail.com>
2019-06-25 15:45:14 -0300

    13: Melhorando sua App com Filtros e Mixin - Filtros

--- 13_filtros_mixins/filtros-mixins-exercicios/src/App.vue
+++ 13_filtros_mixins/filtros-mixins-exercicios/src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="app">
     <h1>Filtros & Mixins</h1>
+    <hr>
+    <p>{{ u_cpf | f_cpf | f_inverter }}</p>
+    <p>{{ u_cpf | f_inverter }}</p>
+    <p>{{ 'ASDFG' | f_inverter }}</p>
+    <hr>
+    <input type="text" :value="u_cpf | f_cpf | f_inverter">
   </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        u_cpf: '12312312300',
+      }
+    },
+    filters: {
+      f_cpf( valor ) {
+        const arr = valor.split( '' )
+        arr.splice( 3, 0, '.' )
+        arr.splice( 7, 0, '.' )
+        arr.splice( 11, 0, '-' )
+        return arr.join( '' )
+      },
+    },
   }
 </script>
 
@@ -18,6 +37,9 @@
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+  input {
     font-size: 2.5rem;
   }
 </style>
--- 13_filtros_mixins/filtros-mixins-exercicios/src/main.js
+++ 13_filtros_mixins/filtros-mixins-exercicios/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter( 'f_inverter', function( valor ) {
+  return valor.split( '' ).reverse().join( '' )
+} )
+
 new Vue( {
   render: h => h( App ),
 } ).$mount( '#app' )
