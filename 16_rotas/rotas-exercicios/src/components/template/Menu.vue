f0208ebdc0160e2e6e78ac47144afada8cb2030a
RafahCSilva <rafahcsilva@gmail.com>
2019-06-26 14:16:31 -0300

    16: Rotas em uma Aplicação VueJS - Nav Menu links

---
+++ 16_rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="menu">
+    <ul>
+      <router-link tag="li" active-class="active" exact to="/"><a>Início</a></router-link>
+      <router-link tag="li" active-class="active" exact to="/usuario"><a>Usuarios</a></router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Menu'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- 16_rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ 16_rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="usuario">
     <h2>Usuário</h2>
+    <hr>
+    <button success @click="irParaInicio">&lt;Voltar</button>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Usuario'
+    name: 'Usuario',
+    methods: {
+      irParaInicio() {
+        // this.$router.push( '/' )
+        this.$router.push( { path: '/' } )
+      }
+    }
   }
 </script>
 
