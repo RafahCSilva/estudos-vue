bc3d99a39164f9f75ab667dcd7c4e07d1893f557
RafahCSilva <rafahcsilva@gmail.com>
2019-06-26 15:12:17 -0300

    16: Rotas em uma Aplicação VueJS - Route com Query

--- 16_rotas/rotas-exercicios/src/components/usuario/UsuarioShow.vue
+++ 16_rotas/rotas-exercicios/src/components/usuario/UsuarioShow.vue
@@ -3,7 +3,11 @@
     <h3>Usuario Detalhe</h3>
     <p><strong>Código:</strong> {{ id }}</p>
     <router-link tag="button" primario :to="`/usuario/${id}/edit`">Editar Url</router-link>
-    <router-link tag="button" primario :to="{ name: 'usuariosEdit', params: { id } }">Editar Named</router-link>
+    <router-link tag="button" primario :to="{
+     name: 'usuariosEdit',
+     params: { id },
+     query: { completo: true, lingua: 'en' }
+     }">Editar Named</router-link>
   </div>
 </template>
 
