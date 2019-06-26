076a6506179c9d7e3ec5536fea601840bf510df8
RafahCSilva <rafahcsilva@gmail.com>
2019-06-26 11:19:03 -0300

    15: Conectando com Servidor via HTTP - post e get

--- 15_http/http-exercicios/src/App.vue
+++ 15_http/http-exercicios/src/App.vue
@@ -1,18 +1,77 @@
 <template>
   <div id="app" class="container">
     <h1>HTTP com Axios</h1>
+    <b-card>
+      <b-form-group label="Nome:">
+        <b-form-input
+          type="text"
+          size="lg"
+          v-model="usuario.nome"
+          placeholder="Informe o Nome"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group label="E-mail:">
+        <b-form-input
+          type="email"
+          size="lg"
+          v-model="usuario.email"
+          placeholder="Informe o E-mail"
+        ></b-form-input>
+      </b-form-group>
+      <hr>
+      <b-button
+        @click="salvar"
+        variant="primary"
+      >Salvar
+      </b-button>
+    </b-card>
+
+    <b-card>
+      <b-button @click="obterUsuarios">Obter Usuários</b-button>
+      <hr>
+      <b-list-group>
+        <b-list-group-item
+          v-for="(usuario, id) in usuarios"
+          :key="id">
+          <strong>ID: </strong>{{ id }} <br>
+          <strong>Nome: </strong>{{ usuario.name }} <br>
+          <strong>E-mail: </strong>{{ usuario.email }}
+        </b-list-group-item>
+      </b-list-group>
+    </b-card>
   </div>
 </template>
 
 <script>
   export default {
-    created() {
-      console.log( 'exec!' )
-      this.$http.post( 'usuarios.json', {
-        nome: 'Maria',
-        email: 'maria@gmail.com'
-      } ).then( ( res ) => { console.log( res ) } )
-    }
+    data() {
+      return {
+        usuario: {
+          nome: '',
+          email: ''
+        },
+        usuarios: []
+      }
+    },
+    methods: {
+      salvar() {
+        console.log( this.usuario )
+        this.$http
+            .post( 'usuarios.json', this.usuario )
+            .then( ( res ) => {
+              console.log( res )
+              this.usuario.nome = ''
+              this.usuario.email = ''
+            } )
+      },
+      obterUsuarios() {
+        this.$http.get( 'usuarios.json' )
+            .then( ( res ) => {
+              this.usuarios = res.data
+              console.log( 'Usuarios:', this.usuarios )
+            } )
+      }
+    },
   }
 </script>
 
