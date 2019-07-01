c8e8fe139218c039007631a168f10f5bcd58b46f
RafahCSilva <rafahcsilva@gmail.com>
2019-07-01 19:03:19 -0300

    18: Pj #3 - The Stock Trader - Componente de Ações (Stock)

--- 18_pj3_the_stock_trader/stock-trader/src/components/stocks/Stocks.vue
+++ 18_pj3_the_stock_trader/stock-trader/src/components/stocks/Stocks.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>Ações</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"/>
+  </v-layout>
 </template>
 
 <script>
+  import Stock from './Stock'
+
   export default {
-    name: 'Stocks'
+    name: 'Stocks',
+    components: {
+      Stock
+    },
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 110 },
+          { id: 2, name: 'Google', price: 200 },
+          { id: 3, name: 'Apple', price: 280 },
+          { id: 4, name: 'Twitter', price: 83 },
+        ]
+      }
+    },
   }
 </script>
 
