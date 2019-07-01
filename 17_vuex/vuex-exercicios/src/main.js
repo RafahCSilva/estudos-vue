2caa80bf6be8d85aa1fe553a976eceecf501937e
RafahCSilva <rafahcsilva@gmail.com>
2019-07-01 15:57:32 -0300

    17: Vuex (base)

---
+++ 17_vuex/vuex-exercicios/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Painel from './components/Painel'
+
+Vue.config.productionTip = false
+
+Vue.component( 'Painel', Painel )
+
+Vue.filter( 'dinheiro', valor => {
+  return `R$ ${ parseFloat( valor ).toFixed( 2 ) }`.replace( '.', ',' )
+} )
+
+new Vue( {
+  render: h => h( App ),
+} ).$mount( '#app' )
