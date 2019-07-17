a05e92391816560db483e044db0ca16a0783b3c0
RafahCSilva <rafahcsilva@gmail.com>
2019-07-17 16:13:42 -0300

    21: Pj #5 - Problema Monty Hall (base)

---
+++ 21_pj5_monty_hall/montyhall/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Problema Monty Hall</h1>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'app',
+    components: {}
+  }
+</script>
+
+<style lang="scss">
+  * {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  body {
+    color: #fff;
+    background: linear-gradient(to right, rgb(21, 152, 87), rgb(21, 87, 153));
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+      border: 1px solid #000;
+      background-color: #0004;
+      padding: 20px;
+      margin-bottom: 60px;
+    }
+  }
+</style>
