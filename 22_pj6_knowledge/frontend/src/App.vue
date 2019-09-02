1881f5a25d330b7fe9fe5b8610a0df6a306281fb
RafahCSilva <rafahcsilva@gmail.com>
2019-09-02 20:24:01 -0300

    22: Pj #6 - Knowledge - Autenticação Validando Token

--- 22_pj6_knowledge/frontend/src/App.vue
+++ 22_pj6_knowledge/frontend/src/App.vue
@@ -4,22 +4,27 @@
       title="RaFaoPress"
       :hide-toggle="!user"
       :hide-user-dropdown="!user" />
-    <Menu v-if="user"/>
-    <Conteudo />
+    <Menu v-if="user" />
+    <Loading v-if="validatingToken"/>
+    <Conteudo v-else/>
     <Footer />
   </div>
 </template>
 
 <script>
   import { mapState } from 'vuex'
+  import axios from 'axios'
   import Header from './components/templates/Header'
   import Menu from './components/templates/Menu'
   import Conteudo from './components/templates/Conteudo'
   import Footer from './components/templates/Footer'
+  import { baseApiUrl, userKey } from '@/global'
+  import Loading from '@/components/templates/Loading'
 
   export default {
     name: 'App',
     components: {
+      Loading,
       Header,
       Menu,
       Conteudo,
@@ -28,6 +33,37 @@
     computed: {
       ...mapState( [ 'isMenuVisible', 'user' ] ),
     },
+    data() {
+      return {
+        validatingToken: true
+      }
+    },
+    methods: {
+      async validateToken() {
+        this.validatingToken = true
+        const json = localStorage.getItem( userKey )
+        const userData = JSON.parse( json )
+        this.$store.commit( 'setUser', null )
+
+        if ( !userData ) {
+          this.validatingToken = false
+          this.$router.push( { name: 'auth' } )
+          return
+        }
+
+        const res = await axios.post( `${ baseApiUrl }/validateToken`, userData )
+        if ( res.data ) {
+          this.$store.commit( 'setUser', userData )
+        } else {
+          localStorage.removeItem( userKey )
+          this.$router.push( { name: 'auth' } )
+        }
+        this.validatingToken = false
+      }
+    },
+    created(){
+      this.validateToken()
+    }
   }
 </script>
 
