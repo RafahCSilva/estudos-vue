2ec97125098cb09375b15f73b76b7b8ea54ef970
RafahCSilva <rafahcsilva@gmail.com>
2019-09-02 20:42:29 -0300

    22: Pj #6 - Knowledge - Autenticação Permissão a Rota

--- 22_pj6_knowledge/frontend/src/config/router.js
+++ 22_pj6_knowledge/frontend/src/config/router.js
@@ -5,18 +5,32 @@ import AdminPages from '@/components/admin/AdminPages'
 import ArticlesByCategory from '@/components/article/ArticlesByCategory'
 import ArticleById from '@/components/article/ArticleById'
 import Auth from '@/components/auth/Auth'
+import { userKey } from '@/global'
 
 Vue.use( VueRouter )
 
 const routes = [
   { name: 'home', path: '/', component: Home },
-  { name: 'adminPages', path: '/admin', component: AdminPages },
+  { name: 'adminPages', path: '/admin', component: AdminPages, meta: { requireAdmin: true } },
   { name: 'articleByCategory', path: '/categories/:id/articles', component: ArticlesByCategory },
   { name: 'articleById', path: '/articles/:id', component: ArticleById },
   { name: 'auth', path: '/auth', component: Auth }
 ]
 
-export default new VueRouter( {
+const router = new VueRouter( {
   mode: 'history',
   routes
 } )
+
+router.beforeEach( ( to, from, next ) => {
+  const json = localStorage.getItem( userKey )
+  const user = JSON.parse( json )
+
+  if ( to.matched.some( record => record.meta.requireAdmin ) ) {
+    user && user.admin ? next() : next( { path: '/' } )
+  }
+
+  return next()
+} )
+
+export default router
